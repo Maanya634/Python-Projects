15eece2bd3382cf833b66769fff9aef614b1aec7
Maanya634 <120562897+Maanya634@users.noreply.github.com>
2022-12-14 17:17:46 +0530

    adding index.py

---
+++ index.py
@@ -0,0 +1,16 @@
+name = input('Enter your name')
+age = input('Enter your age')
+
+import csv
+
+header = ['name', 'area', 'country_code2', 'country_code3']
+data = [name, age]
+
+with open('countries.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write the data
+    writer.writerow(data)
